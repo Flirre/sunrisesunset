211b757f2ed872c85c02850da8be03bd81f96287
Filip Lindahl <gysztaf@gmail.com>
2016-10-05 22:11:11 +0200

    Very intricate way to compare time. Should have used datetime. Might rewrite.

--- sundown.py
+++ sundown.py
@@ -32,8 +32,40 @@ def parseJsonTime(s):
     return slist                  # He is 420 friendly.
 
 
-# print(sunrise())
-# print(sunset())
-# print(timeNow())
+def lightout():
+    sunriseTime = parseJsonTime(sunrise()).split(':')
+    sunsetTime = parseJsonTime(sunset()).split(':')
+    nowTime = timeNow().split(':')
+    print(nowTime[0])
+    print(sunriseTime[0])
+    print(nowTime[0] < sunriseTime[0])
+    if(nowTime[0] > sunriseTime[0]):
+        if(nowTime[0] < sunsetTime[0]):
+            return True
+        else:
+            if(nowTime[0] == sunsetTime[0]):
+                if(nowTime[1] < sunsetTime[1]):
+                    return True
+                if(nowTime[1] == sunsetTime[1]):
+                    if(nowTime[2] < sunsetTime[2]):
+                        return True
+                    else:
+                        return False
+                else:
+                    return False
+            else:
+                return False
+    if(nowTime[0] == sunriseTime[0]):
+        if(nowTime[1] > sunriseTime[1]):
+            return True
+        if(nowTime[1] == sunriseTime[1]):
+            if(nowTime[2] >= sunriseTime[2]):
+                return True
+            else:
+                return False
+        else:
+            return False
 
-print(parseJsonTime(sunrise()))
+    else:
+        return False
+print(lightout())

d8627c07cfb9e7382a594c5b3deab4d1c218d345
Filip Lindahl <gysztaf@gmail.com>
2016-10-06 13:02:24 +0200

    Shorter implementation that actually works.

--- sundown.py
+++ sundown.py
@@ -33,39 +33,10 @@ def parseJsonTime(s):
 
 
 def lightout():
-    sunriseTime = parseJsonTime(sunrise()).split(':')
-    sunsetTime = parseJsonTime(sunset()).split(':')
-    nowTime = timeNow().split(':')
-    print(nowTime[0])
-    print(sunriseTime[0])
-    print(nowTime[0] < sunriseTime[0])
-    if(nowTime[0] > sunriseTime[0]):
-        if(nowTime[0] < sunsetTime[0]):
-            return True
-        else:
-            if(nowTime[0] == sunsetTime[0]):
-                if(nowTime[1] < sunsetTime[1]):
-                    return True
-                if(nowTime[1] == sunsetTime[1]):
-                    if(nowTime[2] < sunsetTime[2]):
-                        return True
-                    else:
-                        return False
-                else:
-                    return False
-            else:
-                return False
-    if(nowTime[0] == sunriseTime[0]):
-        if(nowTime[1] > sunriseTime[1]):
-            return True
-        if(nowTime[1] == sunriseTime[1]):
-            if(nowTime[2] >= sunriseTime[2]):
-                return True
-            else:
-                return False
-        else:
-            return False
+    sunriseTime = datetime.datetime.strptime(parseJsonTime(sunrise()), "%H:%M:%S")
+    sunsetTime = datetime.datetime.strptime(parseJsonTime(sunset()), "%H:%M:%S")
+    nowTime = timeNow()
+    return (nowTime >= sunriseTime and nowTime <= sunsetTime)
 
-    else:
-        return False
+
 print(lightout())
